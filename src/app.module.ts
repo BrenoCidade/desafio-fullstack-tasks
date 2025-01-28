51ee319dc7fbdb1ca544bbb35c8ea860404e5ef0
brenocidade <brenocidade01@gmail.com>
2025-01-28 00:17:34 -0300

    feat: implementando arquivos de config do prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { PrismaService } from './database/prisma.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [TasksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
