33a7e272006637abcdb31067ba7f7c82297e0668
brenocidade <brenocidade01@gmail.com>
2025-01-28 00:24:36 -0300

    feat: implementando update e create taskDtos e implementando o swagger

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,26 @@
-export class CreateTaskDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator'
+
+export class CreateTaskDto {
+    @ApiProperty({
+        description: 'Titulo da task',
+        example: 'Jogar bola',
+    })
+    @IsNotEmpty({ message: 'O campo title não pode estar vazio' })
+    @IsString()
+    title: string;
+
+    @ApiProperty({
+        description: 'Descrição da task',
+        example: 'Irei jogar bola com meus amigos',
+    })
+    @IsString()
+    description: string;
+
+    @ApiProperty({
+        description: 'Status da task',
+        example: 'pendente',
+    })
+    @IsBoolean()
+    status: boolean;
+}
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,30 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+    @ApiProperty({
+        description: 'Titulo da task',
+        example: 'Jogar bola',
+    })
+    @IsString()
+    @IsOptional()
+    title: string;
+
+    @ApiProperty({
+        description: 'Descrição da task',
+        example: 'Irei jogar bola com meus amigos',
+    })
+    @IsOptional()
+    @IsString()
+    description: string;
+
+    @ApiProperty({
+        description: 'Status da task',
+        example: 'pendente',
+    })
+    @IsOptional()
+    @IsBoolean()
+    status: boolean;
+}
